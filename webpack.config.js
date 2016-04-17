1587d3545831f025729671f48a87bfab88eeaaac
byzg <byzg@mail.ru>
2016-04-18 02:44:33 +0300

    2.8 actions

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require('path')
 var webpack = require('webpack')
-var NpmInstallPlugin = require('npm-install-webpack-plugin')
+// var NpmInstallPlugin = require('npm-install-webpack-plugin')
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
@@ -16,8 +16,8 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new NpmInstallPlugin()
+    new webpack.HotModuleReplacementPlugin()
+    // new NpmInstallPlugin()
   ],
   module: {
     preLoaders: [
